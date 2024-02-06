8c05b37c61fd0c3182496283fabfe27cedf7695e
Guilherme Arruda <guilherme_arruda@live.com>
2024-02-06 00:38:33 -0300

    arquivos renomeados ou movidos

--- src/GetRide.ts
+++
@@ -1,9 +0,0 @@
-import RideDAO from "./RideDAO";
-
-export default class GetRide {
-    constructor(readonly rideDao: RideDAO) {}
-
-    async execute (rideId: string) {
-        return await this.rideDao.getByRideId(rideId)
-    }
-}
